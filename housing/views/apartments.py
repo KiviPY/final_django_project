5b3073f07a7828bf171e9fb3a8413fe626691f9b
KiviPY <kirillvigutov@gmail.com>
2026-04-20 23:08:42 +0200

    Add paginations

--- housing/views/apartments.py
+++ housing/views/apartments.py
@@ -1,3 +1,4 @@
+from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
 
@@ -11,9 +12,9 @@ class ApartmentListCreateAPI(generics.ListCreateAPIView):
     queryset = RegisterApartments.objects.all()
     serializer_class = RegisterApartmentsSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # любой может смотреть, только авторизованные могут создать
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_class = ApartmentFilter
-
+    ordering_fields = ['average_rating', 'created_at', 'views_count', 'price_per_month']
 
 class ApartmentRetrieveUpdateDestroyAPI(generics.RetrieveUpdateDestroyAPIView):
     queryset = RegisterApartments.objects.all()
